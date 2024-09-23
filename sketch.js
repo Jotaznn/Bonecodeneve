8e180ab26efdcae92136e70383d4e39d13738a08
Jotaznn <111324206+Jotaznn@users.noreply.github.com>
2024-09-23 15:36:47 -0300

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,16 @@
+function setup(){
+createCanvas(400,400)
+}
+
+
+
+function draw() {
+  background(220)
+  fill(135,206,235)
+  rect(0,0,400,200)
+  fill("yellow")
+  circle(260,200,80)
+  fill(70,130,180)
+  rect(0,200,400,200)
+  
+}
